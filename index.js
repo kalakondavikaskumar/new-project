bb9cdc02e70ead75e29f07a05d672693fd96db43
kalakondavikaskumar <113631243+kalakondavikaskumar@users.noreply.github.com>
2022-11-20 20:43:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,46 @@
 const http = require("http");
 const fs = require("fs");
-
+const args = require('minimist')(process.argv.slice(2));
+const port = args.port;
 let homeContent = "";
 let projectContent = "";
+let registrationContent = "";
 
 fs.readFile("home.html", (err, home) => {
-  if (err) {
-    throw err;
-  }
-  homeContent = home;
+    if (err) {
+        throw err;
+    }
+    homeContent = home;
 });
-
 fs.readFile("project.html", (err, project) => {
-  if (err) {
-    throw err;
-  }
-  projectContent = project;
+    if (err) {
+        throw err;
+    }
+    projectContent = project;
 });
-http
-  .createServer((request, response) => {
+fs.readFile("registration.html", (err, registration) => {
+    if (err) {
+        throw err;
+    }
+    registrationContent = registration;
+});
+http.createServer((request, response) => {
     let url = request.url;
+    // let port = request.port;
     response.writeHeader(200, { "Content-Type": "text/html" });
     switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
+        case "/project":
+            response.write(projectContent);
+            response.end();
+            break;
+        case "/registration":
+            response.write(registrationContent);
+            response.end();
+            break;
+        default:
+            response.write(homeContent);
+            response.end();
+            break;
     }
-  })
-  .listen(5000);
+})
+    .listen(port);
